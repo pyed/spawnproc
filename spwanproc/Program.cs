c82ffc31b32efeb2007134ff63a3ec57c2a1e57c
pyed <iabdulelah@gmail.com>
2015-06-27 05:19:37 +0300

    init

---
+++ spwanproc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using murrayju.ProcessExtensions;
+
+namespace spwanproc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running Calc.exe");
+            ProcessExtensions.StartProcessAsCurrentUser("calc.exe");
+            Console.ReadKey();
+        }
+    }
+}
